3dbb0824eebf6a94ab7fdcc2f04820917996948a
Dave Quick <dave.quick@oracle.com>
2018-12-13 23:11:42 -0800

    OK - hoooked up kafka -> morph json to something nicer -> elasticsearch to make demo showing data in elasticsearch from osquery -> kafka -> elasticsearch -> kibana working

--- kafka/console-subscribe.sh
+++ kafka/console-subscribe.sh
@@ -1,1 +1,1 @@
-docker exec demo-osquery_kafka_1 /opt/kafka_2.11-0.10.0.0/bin/kafka-console-consumer.sh --zookeeper=zookeeper:2181 --topic=OsQueryD
+docker exec demo-osqueryd_kafka_1 /opt/kafka_2.11-0.10.0.0/bin/kafka-console-consumer.sh --zookeeper=zookeeper:2181 --topic=OsQueryD
